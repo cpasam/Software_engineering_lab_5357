8ab9e980083fdb8685f7a44c76ecb8ee7c4f8415
cpasam <cpasam@gitam.in>
2025-08-21 10:59:52 +0530

    1st program uploaded

---
+++ version1_hardcoded.py
@@ -0,0 +1,8 @@
+def quadratic_temperature(a, b, c, t):
+    return a * (t ** 2) + b * t + c
+
+a, b, c = 1, -2, 30
+t = 5
+
+temperature = quadratic_temperature(a, b, c, t)
+print(f"At time t={t}, Predicted Temperature = {temperature}")
