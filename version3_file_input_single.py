a911337ee9f05c3c8d04beda48bdd5e9ca1be96b
cpasam <cpasam@gitam.in>
2025-08-21 11:02:41 +0530

    3rd program uploaded

---
+++ version3_file_input_single.py
@@ -0,0 +1,9 @@
+def quadratic_temperature(a, b, c, t):
+    return a * (t ** 2) + b * t + c
+
+with open("inputs_single.txt", "r") as f:
+    line = f.readline().strip()
+    a, b, c, t = map(float, line.split())
+
+temperature = quadratic_temperature(a, b, c, t)
+print(f"At time t={t}, Predicted Temperature = {temperature}")
